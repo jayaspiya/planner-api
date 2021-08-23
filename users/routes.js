d9327913f734b7fed4436f228568dd6f0f445f26
zayazzp <pzayazz@gmail.com>
2021-08-23 17:08:02 +0545

    :rocket: Changed Login GET method to POST

--- users/routes.js
+++ users/routes.js
@@ -3,7 +3,7 @@ const controller = require("./controller.js")
 const auth = require("../utils/auth.js")
 
 router.post("/register", controller.register_new_user)
-router.get("/login", controller.login_user)
+router.post("/login", controller.login_user)
 
 // User Auth
 router.get("/", auth, controller.get_profile)
